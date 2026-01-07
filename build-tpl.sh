b701edeabd3d76180e854eaade66960c0b215235
Wang Jie <i@i8e.net>
2026-01-07 18:11:45 +0800

    support building android template

--- build-tpl.sh
+++ build-tpl.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# vim: set et sw=2 ts=2:
 
 set -xe
 
@@ -9,96 +8,146 @@ _ENCRYPTION_KEY_FILE="$_RELEASE_DIR/ir-ios.gdkey"
 _TPL_URL="https://downloads.godotengine.org/?version=4.5.1&flavor=stable&slug=export_templates.tpz&platform=templates"
 _TPL_FILE="$_RELEASE_DIR/export_templates.tpz"
 
+_SWAPPY_URL="https://github.com/godotengine/godot-swappy/releases/download/from-source-2025-01-31/godot-swappy.zip"
+_SWAPPY_DIR="thirdparty/swappy-frame-pacing"
+
 # ensure encryption key exists
 if [ ! -f $_ENCRYPTION_KEY_FILE ]; then
-  echo "Encryption key not found!"
-  exit 1
+	echo "Encryption key not found!"
+	exit 1
 fi
 
 _REQUIRED_TOOLS=(scons zip wget unzip)
 
 # ensure tools are installed
 for tool in ${_REQUIRED_TOOLS[@]}; do
-  if ! command -v $tool &> /dev/null; then
-    echo "$tool not found!"
-    exit 1
-  fi
+	if ! command -v $tool &> /dev/null; then
+		echo "$tool not found!"
+		exit 1
+	fi
 done
 
 
 function apply_patch() {
-  # Apply any necessary patches here
-  echo "Patching source code..."
-  patch -V none -u core/io/file_access_encrypted.cpp -i release/key_magic.patch
+	# Apply any necessary patches here
+	echo "Patching source code..."
+	patch -V none -u core/io/file_access_encrypted.cpp -i release/key_magic.patch
 }
 
 function revert_patch() {
-  # Revert any patches applied
-  echo "Reverting patches..."
-  patch -R -s -u core/io/file_access_encrypted.cpp -i release/key_magic.patch
+	# Revert any patches applied
+	echo "Reverting patches..."
+	patch -R -s -u core/io/file_access_encrypted.cpp -i release/key_magic.patch
 }
 
 function build_editor() {
-  echo "Building Godot..."
-  scons \
-    target=editor \
-	  production=yes \
-    platform=ios
+	echo "Building Godot..."
+	scons \
+		target=editor \
+		production=yes
 }
 
+_TEMPLATE_BUILD_COMMON_ARGS=(
+	"production=yes"
+	"disable_3d=yes"
+	"disable_advanced_gui=yes"
+	"disable_physics_3d=yes"
+	"disable_navigation_3d=yes"
+	"disable_xr=yes"
+)
+
 function build_ios() {
-  export SCRIPT_AES256_ENCRYPTION_KEY=$(cat $_ENCRYPTION_KEY_FILE | xargs)
+	export SCRIPT_AES256_ENCRYPTION_KEY=$(cat $_ENCRYPTION_KEY_FILE | xargs)
 
-  _TARGETS=(
-    template_release
-    template_debug
-  )
+	_TARGETS=(
+		template_release
+		template_debug
+	)
 
-  for _TARGET in ${_TARGETS[@]}; do
-    echo "Building iOS $_TARGET"
-    scons \
-      arch=arm64 \
-      target=$_TARGET \
-      production=yes \
-      disable_3d=yes \
-      disable_advanced_gui=yes \
-      disable_physics_3d=yes \
-      disable_navigation_3d=yes \
-      disable_xr=yes \
-      platform=ios
-  done
+	for _TARGET in ${_TARGETS[@]}; do
+		echo "Building iOS $_TARGET"
+		scons \
+			arch=arm64 \
+			target=$_TARGET \
+			"${_TEMPLATE_BUILD_COMMON_ARGS[@]}" \
+			platform=ios
+	done
 
-  _IOS_TPL_DIR="release/ios"
+	_IOS_TPL_DIR="release/ios"
 
-  # ensure template dir exists
-  if [ ! -d "$_IOS_TPL_DIR" ]; then
-    if [ ! -f "$_TPL_FILE" ]; then
-      echo "Downloading templates package..."
-      wget -O "$_TPL_FILE" "$_TPL_URL"
-    fi
+	# ensure template dir exists
+	if [ ! -d "$_IOS_TPL_DIR" ]; then
+		if [ ! -f "$_TPL_FILE" ]; then
+			echo "Downloading templates package..."
+			wget -O "$_TPL_FILE" "$_TPL_URL"
+		fi
 
-    unzip $_TPL_FILE -d $_RELEASE_DIR
-    unzip $_RELEASE_DIR/templates/ios.zip -d $_IOS_TPL_DIR
-  fi
+		unzip $_TPL_FILE -d $_RELEASE_DIR
+		unzip $_RELEASE_DIR/templates/ios.zip -d $_IOS_TPL_DIR
+	fi
 
-  cp bin/libgodot.ios.template_release.arm64.a \
-    $_IOS_TPL_DIR/libgodot.ios.release.xcframework/ios-arm64/libgodot.a
+	cp bin/libgodot.ios.template_release.arm64.a \
+		$_IOS_TPL_DIR/libgodot.ios.release.xcframework/ios-arm64/libgodot.a
 
-  cp bin/libgodot.ios.template_debug.arm64.a \
-    $_IOS_TPL_DIR/libgodot.ios.debug.xcframework/ios-arm64/libgodot.a
+	cp bin/libgodot.ios.template_debug.arm64.a \
+		$_IOS_TPL_DIR/libgodot.ios.debug.xcframework/ios-arm64/libgodot.a
 
-  cd $_IOS_TPL_DIR
-  zip -r ../ios.zip ./*
-  cd $_CURRENT_DIR
+	cd $_IOS_TPL_DIR
+	zip -r ../ios.zip ./*
+	cd $_CURRENT_DIR
+}
+
+function _prepare_swappy() {
+	local _ANDROID_ARCHS=("arm64-v8a" "armeabi-v7a" "x86" "x86_64")
+	local _OK="YES"
+
+	# determine if swappy is already prepared
+	for arch in ${_ANDROID_ARCHS[@]}; do
+		if [ ! -f $_SWAPPY_DIR/$arch/libswappy_static.a ]; then
+			_OK="NO"
+		fi
+	done
+	if [ "$_OK" == "YES" ]; then
+		return
+	fi
+
+	# download and extract godot-swappy
+	wget -O "$_RELEASE_DIR/godot-swappy.zip" "$_SWAPPY_URL"
+	unzip -o $_RELEASE_DIR/godot-swappy.zip -d "$_RELEASE_DIR/godot-swappy"
+	rm -f $_RELEASE_DIR/godot-swappy.zip
+
+	# copy each swappy files to thirdparty
+	for arch in ${_ANDROID_ARCHS[@]}; do
+		cp -r $_RELEASE_DIR/godot-swappy/$arch \
+			$_SWAPPY_DIR
+	done
+
+	rm -rf $_RELEASE_DIR/godot-swappy
+}
+
+function build_android() {
+	_prepare_swappy
+
+	export SCRIPT_AES256_ENCRYPTION_KEY=$(cat $_ENCRYPTION_KEY_FILE | xargs)
+
+	scons "${_TEMPLATE_BUILD_COMMON_ARGS[@]}" platform=android target=template_debug arch=arm32
+	scons "${_TEMPLATE_BUILD_COMMON_ARGS[@]}" platform=android target=template_debug arch=arm64 generate_android_binaries=yes
+
+	scons "${_TEMPLATE_BUILD_COMMON_ARGS[@]}" platform=android target=template_release arch=arm32
+	scons "${_TEMPLATE_BUILD_COMMON_ARGS[@]}" platform=android target=template_release arch=arm64
+	scons "${_TEMPLATE_BUILD_COMMON_ARGS[@]}" platform=android target=template_release arch=x86_32
+	scons "${_TEMPLATE_BUILD_COMMON_ARGS[@]}" platform=android target=template_release arch=x86_64 generate_android_binaries=yes
 }
 
 apply_patch
 trap revert_patch EXIT
 if [ "$1" == "ios" ]; then
-  build_ios
+	build_ios
+elif [ "$1" == "android" ]; then
+	build_android
 elif [ "$1" == "editor" ]; then
-  build_editor
+	build_editor
 else
-  echo "Unknown platform: $1"
-  exit 1
+	echo "Unknown platform: $1"
+	exit 1
 fi
